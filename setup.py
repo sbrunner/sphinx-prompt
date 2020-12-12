a64b58e80b719058301a80893d07ee4385611d14
Stéphane Brunner <stephane.brunner@camptocamp.com>
2020-12-12 15:45:27 +0100

    Add check, reformat, remove Python 2 support

--- setup.py
+++ setup.py
@@ -1,33 +1,31 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 install_requires = [
-    'Sphinx',
-    'pygments',
+    "Sphinx",
+    "pygments",
 ]
-setup_requires = [
-]
-tests_require = [
-]
+setup_requires = []
+tests_require = []
 
 setup(
-    name='sphinx-prompt',
-    version='1.3.0',
+    name="sphinx-prompt",
+    version="1.3.0",
     description="Sphinx directive to add unselectable prompt",
     long_description="`Sphinx directive to add unselectable prompt <https://github.com/sbrunner/sphinx-prompt>`_",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
     ],
-    author='Stéphane Brunner',
-    author_email='stephane.brunner@camptocamp.com',
-    url='https://github.com/sbrunner/sphinx-prompt',
-    license='BSD',
-    keywords='gis tilecloud chain',
+    author="Stéphane Brunner",
+    author_email="stephane.brunner@camptocamp.com",
+    url="https://github.com/sbrunner/sphinx-prompt",
+    license="BSD",
+    keywords="gis tilecloud chain",
     packages=find_packages(exclude=["*.tests", "*.tests.*"]),
     include_package_data=True,
     zip_safe=False,
--- sphinx-prompt/__init__.py
+++ sphinx-prompt/__init__.py
@@ -4,17 +4,12 @@
 
 from docutils import nodes
 from docutils.parsers import rst
-
 from pygments import highlight
-from pygments.lexers import TextLexer
-from pygments.lexers import BashLexer
-from pygments.lexers import PythonLexer
-from pygments.lexers import ScalaLexer
 from pygments.formatters import HtmlFormatter
+from pygments.lexers import BashLexer, PythonLexer, ScalaLexer, TextLexer
 
 
 class PromptCache:
-
     def __init__(self):
         self.clear()
 
@@ -25,7 +20,7 @@ class PromptCache:
 
     def register_prompt(self, prompt):
         if prompt in self.prompts:
-            return ''
+            return ""
         else:
             index = self.next_index
             self.next_index = index + 1
@@ -33,10 +28,12 @@ class PromptCache:
             return """span.prompt{0:d}:before {{
   content: "{1!s} ";
 }}
-""".format(index, prompt)
+""".format(
+                index, prompt
+            )
 
     def get_prompt_class(self, prompt):
-        return 'prompt{0:d}'.format(self.prompts[prompt])
+        return "prompt{0:d}".format(self.prompts[prompt])
 
 
 cache = PromptCache()
@@ -50,7 +47,7 @@ class PromptDirective(rst.Directive):
     def run(self):
         self.assert_has_content()
 
-        language = 'text'
+        language = "text"
         prompt = None
         modifiers = []
 
@@ -58,52 +55,50 @@ class PromptDirective(rst.Directive):
             language = self.arguments[0]
             if len(self.arguments) > 1:
                 prompt = self.arguments[1]
-            elif language == 'bash':
-                prompt = '$'
+            elif language == "bash":
+                prompt = "$"
             if len(self.arguments) > 2:
-                modifiers = self.arguments[2].split(',')
-            if 'auto' in modifiers:
-                prompts = prompt.split(',')
+                modifiers = self.arguments[2].split(",")
+            if "auto" in modifiers:
+                prompts = prompt.split(",")
 
         html = '<div class="highlight-default notranslate"><div class="highlight"><pre>'
-        styles = ''
-        if 'auto' in modifiers:
+        styles = ""
+        if "auto" in modifiers:
             for prompt in prompts:
                 styles += cache.register_prompt(prompt)
         else:
             if prompt is not None:
                 styles += cache.register_prompt(prompt)
         if styles:
-            html += '<style type="text/css">\n' + styles + '</style>'
+            html += '<style type="text/css">\n' + styles + "</style>"
         latex = "\\begin{Verbatim}[commandchars=\\\\\\{\\}]"
 
         Lexer = TextLexer
-        if language == 'bash':
+        if language == "bash":
             Lexer = BashLexer
-        elif language == 'python':
+        elif language == "python":
             Lexer = PythonLexer
-        elif language == 'scala':
+        elif language == "scala":
             Lexer = ScalaLexer
 
         statement = []
-        if 'auto' in modifiers:
-            prompt_class = ''
+        if "auto" in modifiers:
+            prompt_class = ""
             for line in self.content:
-                latex += '\n' + line
+                latex += "\n" + line
 
                 for prompt in prompts:
                     if line.startswith(prompt):
                         if len(statement) > 0:
                             html += '<span class="{0!s}">{1!s}</span>\n'.format(
                                 prompt_class,
-                                highlight(
-                                    '\n'.join(statement),
-                                    Lexer(),
-                                    HtmlFormatter(nowrap=True)
-                                ).strip('\r\n')
+                                highlight("\n".join(statement), Lexer(), HtmlFormatter(nowrap=True)).strip(
+                                    "\r\n"
+                                ),
                             )
                             statement = []
-                        line = line[len(prompt):].rstrip()
+                        line = line[len(prompt) :].rstrip()
                         prompt_class = cache.get_prompt_class(prompt)
                         break
 
@@ -112,57 +107,45 @@ class PromptDirective(rst.Directive):
             if len(statement) > 0:
                 html += '<span class="{0!s}">{1!s}</span>\n'.format(
                     prompt_class,
-                    highlight(
-                        '\n'.join(statement),
-                        Lexer(),
-                        HtmlFormatter(nowrap=True)
-                    ).strip('\r\n')
+                    highlight("\n".join(statement), Lexer(), HtmlFormatter(nowrap=True)).strip("\r\n"),
                 )
-        elif language in ['bash', 'python']:
+        elif language in ["bash", "python"]:
             for line in self.content:
                 statement.append(line)
-                if len(line) == 0 or not line[-1] == '\\':
+                if len(line) == 0 or not line[-1] == "\\":
                     html += '<span class="{0!s}">{1!s}</span>\n'.format(
                         cache.get_prompt_class(prompt),
-                        highlight(
-                            '\n'.join(statement),
-                            Lexer(),
-                            HtmlFormatter(nowrap=True)
-                        ).strip('\r\n')
+                        highlight("\n".join(statement), Lexer(), HtmlFormatter(nowrap=True)).strip("\r\n"),
                     )
                     if prompt is not None:
-                        latex += '\n{0!s} {1!s}'.format(prompt, '\n'.join(statement))
+                        latex += "\n{0!s} {1!s}".format(prompt, "\n".join(statement))
                     else:
-                        latex += '\n' + '\n'.join(statement)
+                        latex += "\n" + "\n".join(statement)
                     statement = []
         else:
             for line in self.content:
                 html += '<span class="{0!s}">{1!s}</span>\n'.format(
                     cache.get_prompt_class(prompt),
-                    highlight(
-                        line,
-                        Lexer(),
-                        HtmlFormatter(nowrap=True)
-                    ).strip('\r\n')
+                    highlight(line, Lexer(), HtmlFormatter(nowrap=True)).strip("\r\n"),
                 )
                 if prompt is not None:
-                    latex += '\n{0!s} {1!s}'.format(prompt, line)
+                    latex += "\n{0!s} {1!s}".format(prompt, line)
                 else:
-                    latex += '\n' + line
+                    latex += "\n" + line
 
         html += "</pre></div></div>"
         latex += "\n\\end{Verbatim}"
 
         return [
-            nodes.raw('\n'.join(self.content), html, format="html"),
-            nodes.raw('\n'.join(self.content), latex, format="latex"),
+            nodes.raw("\n".join(self.content), html, format="html"),
+            nodes.raw("\n".join(self.content), latex, format="latex"),
         ]
 
 
 def setup(app):
-    app.add_directive('prompt', PromptDirective)
-    app.connect('env-purge-doc', cache.clear)
+    app.add_directive("prompt", PromptDirective)
+    app.connect("env-purge-doc", cache.clear)
     return {
-        'parallel_read_safe': True,
-        'parallel_write_safe': True,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
     }
